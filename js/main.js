7ba811e04df9f91589575cdab9dad52f80eb0ccf
Finn <anthony.torres0926@gmail.com>
2019-10-25 14:27:03 -0600

    HTML grid setup. Variables setup.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('Test');
+//constants--
+const player1 = 'X'
+const player2 = 'O'
+
+//variables
+let image, grid, cell, cellALL, winner, loser, score, play, quit;
+
+//cache--
+const cellAll = document.getElementById('cell');
+const reset = document.querySelectorAll('reset');
+const board = document.querySelectorAll('board');
+
+//event--
+cellAll.addEventListener('click', cellClick);
+
+function cellClick(evt) {
+    
+}
