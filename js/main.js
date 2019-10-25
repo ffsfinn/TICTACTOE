6a9d1ea3c9358f72c3275f549ceccad8e092add2
Finn <anthony.torres0926@gmail.com>
2019-10-25 17:00:59 -0600

    Working click function for each cell

--- js/main.js
+++ js/main.js
@@ -3,16 +3,15 @@ const player1 = 'X'
 const player2 = 'O'
 
 //variables
-let image, grid, cell, cellALL, winner, loser, score, play, quit;
+let image, grid, cell, winner, loser, score, play, quit;
 
 //cache--
-const cellAll = document.getElementById('cell');
+const cellAll = document.getElementById('board');
 const reset = document.querySelectorAll('reset');
-const board = document.querySelectorAll('board');
 
 //event--
 cellAll.addEventListener('click', cellClick);
 
 function cellClick(evt) {
-    
-}
+    console.log('hello');
+};
